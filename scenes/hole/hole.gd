87563a3b6270a8b433b02e7a5ccddad7650dce8d
WyattMacKay <wyattmackaydawson@outlook.com>
2025-07-09 10:44:01 -0300

    improve hole physics.

--- scenes/hole/hole.gd
+++ scenes/hole/hole.gd
@@ -4,26 +4,35 @@ var ball: RigidBody2D = null
 
 @export var outer_pull := 250
 @export var inner_pull := 200
-@export var min_distance := 10
+@export var min_velocity := 10
+@export var timer: Timer
 
 var in_inner_pull_range := false
+var putt_sank := false
+
+func _ready() -> void:
+	timer.connect("timeout", ball_sank)
 
 func _physics_process(delta: float) -> void:
 	if ball:
 		var dir = self.global_position - ball.global_position
-		#var len = dir.length()
-		#if ball.linear_velocity.length() > ball.max_power/5:		#Just doesn't feel good
-		if dir.length() > 1:
+		if putt_sank:
+			ball.position = ball.position.move_toward(self.global_position, 50 * delta)
+		
+		elif dir.length() > 1:
 			if in_inner_pull_range:
-				if ball.linear_velocity.length() < min_distance:
-					ball.linear_velocity = ball.linear_velocity.move_toward(dir, inner_pull * delta * 10)
-					
-				#ball.apply_force(dir.normalized() * inner_pull) #apply force at 1/distance
+				if !putt_sank and ball.linear_velocity.length() < min_velocity:
+					putt_sank = true
+					timer.start()
 				else:
 					ball.linear_velocity = ball.linear_velocity.move_toward(Vector2.ZERO, inner_pull * delta)
-					print(dir.length())
 			else:
 				ball.apply_force(dir.normalized() * outer_pull)
+
+
+func ball_sank():
+	print("You did it!")
+	Global.reload()
 
 
 func _on_outer_hole_body_entered(body: Node2D) -> void:
@@ -32,6 +41,7 @@ func _on_outer_hole_body_entered(body: Node2D) -> void:
 
 func _on_outer_hole_body_exited(_body: Node2D) -> void:
 	ball = null
+	putt_sank = false
 
 
 func _on_inner_hole_body_entered(_body: Node2D) -> void:
