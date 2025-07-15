b49999a404b5ee598daa98e6dffbd1bb77270e75
WyattMacKay <wyattmackaydawson@outlook.com>
2025-07-15 19:26:03 -0300

    add basic level end screen. needs improvement.

--- global.gd
+++ global.gd
@@ -2,7 +2,14 @@ extends Node
 
 var ball: RigidBody2D = null
 var ball_start_pos: Vector2 = Vector2.ZERO
+var ui: CanvasLayer = null
 var score_label: Label = null 
+var menu: Control = null
+
+func set_ui(ui_in: CanvasLayer) -> void:
+	ui = ui_in
+	set_label(ui.label)
+	menu = ui.menu
 
 func set_label(l: Label) -> void:
 	score_label = l
@@ -19,6 +26,25 @@ func set_ball(b: Node2D) -> void:
 func initialize_label() -> void:
 	ball.fired.connect(score_label._on_ball_fired.bind())
 	score_label.set_label()
+
+func win() -> void:
+	var menu_label: Label = menu.label
+	var score: int = score_label.score
+	var par: int = score_label.par
+	var golf_term: String
+	match score - par:
+		-4: golf_term = "!!CONDOR!!"
+		-3: golf_term = "ALBATROSS!!!"
+		-2: golf_term = "EAGLE!!"
+		-1: golf_term = "BIRDIE!"
+		0: golf_term = "PAR"
+		1: golf_term = "BOGEY"
+		2: golf_term = "DOUBLE BOGEY"
+		3: golf_term = "TRIPLE BOGEY"
+		_: golf_term = str(score - par) + " strokes"
+	golf_term += "\nPAR: %d\nYOUR SCORE:%d" % [par, score]
+	menu_label.text = golf_term
+	ui.show_menu()
 
 func reload() -> void:
 	get_tree().reload_current_scene()
--- scenes/basic/user_interface/label.gd
+++ scenes/basic/user_interface/label.gd
@@ -1,14 +1,14 @@
 extends Label
 
 @export var par := 3
-var shots := 0
+@export var score := 0
 
 func _ready() -> void:
 	Global.set_label(self)
 
 func _on_ball_fired() -> void:
-	shots += 1
+	score += 1
 	set_label()
 
 func set_label() -> void:
-	text = "Par: %d\nShots: %d" % [par,shots]
+	text = "Par: %d\nShots: %d" % [par,score]
